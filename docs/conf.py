c3fb9461ed1172b558aabc00172eae9067c5a68c
pythzon <45112493+pythzon@users.noreply.github.com>
2023-10-29 07:28:52 -0700

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,30 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
+# -- Project information -----------------------------------------------------
+
+project = 'osifinanceAPI'
+copyright = '2023, OSI Finance Inc.'
+author = 'OSI Finance Inc.'
+
+# The short X.Y version
+version = '0.1'
+# The full version, including alpha/beta/rc tags
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    # Add any Sphinx extensions that you're using.
+]
+
+templates_path = ['_templates']
+
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'alabaster'  # You can change the theme for something else.
+
+html_static_path = ['_static']
