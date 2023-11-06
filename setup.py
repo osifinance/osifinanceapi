ebe8af6b8302a604c1dee908b2a6defa8f15a8a6
ZachRubin <zachxrubin@gmail.com>
2023-11-06 10:48:10 -0600

    state_taxes, readme

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='osifinanceAPI',
-    version='0.1.0',
+    version='0.1.1',
     packages=['osifinanceAPI'],
     description='Python toolkit for interacting with the OSI Finance API',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    python_requires='>=3.7',
-    install_requires=['pandas>=1.3.5']
+    python_requires='>=3.6',
+    install_requires=['pandas>=1.1.5']
 )
